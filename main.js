3c2bf8cec90950da092fa2d5a1aacc4604c4e8ab
beanjo55 <benschweitzer24@gmail.com>
2020-12-22 01:00:50 -0500

    actual stuff

--- main.js
+++ main.js
@@ -1,1 +1,16 @@
 "use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var eris_1 = require("eris");
+var config = require("./config.json");
+var client = new eris_1.Client(config.token, {
+    intents: ["guilds", "guildMessages"]
+});
+client.on("messageCreate", function (msg) {
+    var num = Math.floor(Math.random() * 1024);
+    if (num % 120 === 0) {
+        msg.channel.createMessage("Hi " + msg.author.mention + ",\n    it looks like its been a while since you last hydrated. Make sure you are drinking water!");
+        msg.channel.createMessage("<:hydrate:790819063428546580>");
+    }
+});
+client.connect();
+client.on("error", function () { });
